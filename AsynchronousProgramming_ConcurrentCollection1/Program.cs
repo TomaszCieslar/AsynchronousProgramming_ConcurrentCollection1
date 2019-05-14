8d34b3cc9bc82f3b0d3e6966bcd6a0d5211dc383
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-05-14 06:47:27 +0200

    Add project files.

---
+++ AsynchronousProgramming_ConcurrentCollection1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsynchronousProgramming_ConcurrentCollection1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
